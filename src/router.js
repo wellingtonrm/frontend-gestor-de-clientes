e9b6de03eb55fc2a8904856968fa79d3b74f5d5e
Wellington Ramos <wrm.net@gmail.com>
2020-01-16 22:22:47 -0300

    init

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PaginaErro from './pages/404'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/index'
+import Login from './pages/auth';
+
+const Router = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/auth" component={Login} />
+
+            <Route component={PaginaErro} />
+        </Switch>
+    </BrowserRouter>
+);
+export default Router;
